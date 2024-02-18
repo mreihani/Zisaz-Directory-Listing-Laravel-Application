2b35ea40c6b33c42e29d103b438ade1037f17aff
Amin <reihani.eng@gmail.com>
2024-02-18 09:02:34 +0330

    Use request ip method

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -33,7 +33,7 @@ class IndexController extends Controller
      * @return \Illuminate\Contracts\View\View
      */
     public function homePage(Request $request) {
-        $ip = $_SERVER['REMOTE_ADDR'];
+        $ip = $request->ip;
         
         return view('frontend.pages.home.index', compact('ip'));
     }
