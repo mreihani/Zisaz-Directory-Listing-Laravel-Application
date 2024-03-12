904b3702d9da47bde777029901ee918a902d96e2
Amin <reihani.eng@gmail.com>
2024-03-12 15:30:28 +0330

    Commit some changes to the header menu

--- resources/views/frontend/layouts/header/auth-modals/my-account/header-auth.blade.php
+++ resources/views/frontend/layouts/header/auth-modals/my-account/header-auth.blade.php
@@ -26,17 +26,7 @@
                 <li><a class="dropdown-item" href="{{route('user.dashboard.account-notifications.index')}}">تنظیمات اطلاع رسانی</a></li>
             </ul>
             </li>
-            <li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">روزمه ساز</a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-                <li><a class="dropdown-item" href="job-board-post-resume-1.html">مرحله 1: اطلاعات پایه</a></li>
-                <li><a class="dropdown-item" href="job-board-post-resume-2.html">مرحله 2: تحصیلات</a></li>
-                <li><a class="dropdown-item" href="job-board-post-resume-3.html">مرحله 3: سابقه کار</a></li>
-                <li><a class="dropdown-item" href="job-board-post-resume-4.html">مرحله 4: مهارت ها</a></li>
-                <li><a class="dropdown-item" href="job-board-post-resume-5.html">مرحله 5: نظرات</a></li>
-            </ul>
-            </li>
             <li><a class="dropdown-item" href="job-board-promotion.html">بروزرسانی آگهی</a></li>
-            <li><a class="dropdown-item" href="job-board-employer-single.html">کارفرما/ کارجو</a></li>
             <div class="dropdown-divider"></div>
             <li>
                 <a class="dropdown-item" href="{{route('logout')}}">
