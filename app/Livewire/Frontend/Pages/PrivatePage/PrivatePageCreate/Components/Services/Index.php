12c98a1e0ddf87faf73d72b006735a4188f09bf1
Amin <reihani.eng@gmail.com>
2024-05-22 04:43:56 +0430

    Test github actions

--- app/Livewire/Frontend/Pages/PrivatePage/PrivatePageCreate/Components/Services/Index.php
+++ app/Livewire/Frontend/Pages/PrivatePage/PrivatePageCreate/Components/Services/Index.php
@@ -50,7 +50,6 @@ class Index extends Component
     private function loadInitialValues() {
         if(is_null($this->privateSiteId)) {
 
-            
             // service item repeater form
             $this->itemTitle = [null];
             $this->itemDescription = [null];
