bc8c5158d101bfe291716fb79068d214c382d47b
Amin <reihani.eng@gmail.com>
2024-07-14 16:04:15 +0330

    Some upgrades visits tables 4

--- app/Jobs/VisitorLogging/VisitorLogging.php
+++ app/Jobs/VisitorLogging/VisitorLogging.php
@@ -33,16 +33,16 @@ class VisitorLogging implements ShouldQueue
         $location = \Location::get($ip) ?: null;
 
         $data = [
-            'url' => $this->incoming['url'] ?? null,
-            'device' => $this->incoming['device'] ?? null,
-            'platform' => $this->incoming['platform'] ?? null,
-            'browser' => $this->incoming['browser'] ?? null,
-            'ip' => $ip ?? null,
-            'user_id' => $this->incoming['user_id'] ?? null,
-            'country' => $location->countryName ?? null,
-            'city' => $location->cityName ?? null,
-            'province' => $location->regionName ?? null,
-            'country_code' => $location->countryCode ?? null,
+            'url' => !empty($this->incoming['url']) ? $this->incoming['url'] : null,
+            'device' => !empty($this->incoming['device']) ? $this->incoming['device'] : null,
+            'platform' => !empty($this->incoming['platform']) ? $this->incoming['platform'] : null,
+            'browser' => !empty($this->incoming['browser']) ? $this->incoming['browser'] : null,
+            'ip' => !empty($ip) ? $ip : null,
+            'user_id' => !empty($this->incoming['user_id']) ? $this->incoming['user_id'] : null,
+            'country' => (!empty($location) && !empty($location->countryName)) ? $location->countryName : null,
+            'city' => (!empty($location) && !empty($location->cityName)) ? $location->cityName : null,
+            'province' => (!empty($location) && !empty($location->regionName)) ? $location->regionName : null,
+            'country_code' => (!empty($location) && !empty($location->countryCode)) ? $location->countryCode : null,
         ];
 
         Visit::create($data);
